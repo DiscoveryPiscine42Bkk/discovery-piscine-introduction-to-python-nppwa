85cf613f074ad20d553bfc587a38a72eaa801780
nppwa <nipapornchomphu70@gmail.com>
2025-06-18 12:01:09 +0000

    addyour_namebook.py

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -1,15 +1,16 @@
-def array_of_names(name_dict):
-    full_names = []
+def array_of_name(name_dict):
+    full_names  = []
     for first, last in name_dict.items():
-        full_names = f"{first.capitalize()} {last.capitalize()}"
-        full_names.append(full_names)
+        full_name = f"{first.capitalize()} {last.capitalize()}"
+        full_names.append(full_name)
     return full_names
+persons = {
+    "thanyaluk": "mungwicha",
+    "nipaporn": "nhomphu",
+    "cake": "wa",
+    "wa": "cake"
 
-persons + {
-    "nipaporn": "chomphu",
-    "warit": "watcharamaitithipat",
-    'wa': "peter",
-    "peter": "wa"
 }
+    
+print(array_of_name(persons))
 
-print(array_of_names(persons))
