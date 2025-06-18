98536c3df38da6ceae1385ea013de9ed0da021d3
nppwa <nipapornchomphu70@gmail.com>
2025-06-18 08:21:23 +0000

    addmathods_everywhere.py

--- cell06/ex04/methods_every.py
+++ cell06/ex04/methods_every.py
@@ -1,8 +1,18 @@
-def add_one(x):
-    x = x + 1
-    return x
+import sys
 
-value = 5
-print(value)
-add_one(value)
-print(value)
+def shrink(i):
+    print(i[:8])
+
+def enlarge(i):
+    print(i + 'z'*(8 - len(i)))
+
+if len(sys.argv) < 2:
+   print("none")
+else:
+    for arg in sys.argv[1:]:
+        if len(arg) > 8:
+            shrink(arg)
+        elif len(arg) < 8:
+            enlarge(arg)
+        else:
+            print(arg)
